d8d286cac7f30f6d68e786ea9c23efbdafd813c7
Amit-TheOne <amit.dew009@gmail.com>
2025-10-01 20:57:42 +0530

    Add: search reducer to the Redux store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./features/authSlice";
 import uploadReducer from "./features/uploadSlice";
+import searchReducer from "./features/searchSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     upload: uploadReducer,
+    search: searchReducer,
   },
 });
 
