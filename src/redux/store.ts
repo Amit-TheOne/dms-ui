d536f60574d281e75d65f8890b21c6bfd13f87a6
Amit-TheOne <amit.dew009@gmail.com>
2025-10-02 00:35:24 +0530

    Add: tag reducer in the Redux store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./features/authSlice";
 import uploadReducer from "./features/uploadSlice";
 import searchReducer from "./features/searchSlice";
+import tagReducer from "./features/tagSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     upload: uploadReducer,
     search: searchReducer,
+    tags: tagReducer,
   },
 });
 
