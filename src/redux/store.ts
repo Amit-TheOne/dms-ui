36b50ac0fe2ecfb81566c8134c8cdac190eb7083
Amit-TheOne <amit.dew009@gmail.com>
2025-10-01 20:07:54 +0530

    Add: upload reducer to the Redux store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./features/authSlice";
+import uploadReducer from "./features/uploadSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    upload: uploadReducer,
   },
 });
 
